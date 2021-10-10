bac0bdb57c5c89bf88dc0d6b35e1a03ee448d0bf
wa1tnr <wa1tnr@users.noreply.github.com>
2021-10-10 21:25:31 +0000

    init gpio and test

--- ArduinoIDE/typedef-aa/typedef_ptr-a/n_sketch-tdefptr-aa.cpp
+++ ArduinoIDE/typedef-aa/typedef_ptr-a/n_sketch-tdefptr-aa.cpp
@@ -5,9 +5,26 @@
 
 void (*func)(void); // compiles as-is!
 
+void test_gpio_TEMP(void) {
+    digitalWrite (LED_BUILTIN, 1);
+    delay(100);
+    digitalWrite (LED_BUILTIN, 0);
+    delay(500);
+}
+
+void init_gpio (void) {
+    pinMode (LED_BUILTIN, 1);
+    digitalWrite (LED_BUILTIN, 0);
+}
+
+#define blinks 7
 void setup(void) {
-    // statements
+    init_gpio ();
+    delay(3000);
+    for (int count = blinks; count >0; count--) {
+        test_gpio_TEMP();
     }
+}
 
 void loop(void) {
     // statements
