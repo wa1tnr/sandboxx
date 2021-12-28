27e3dd91c80eabf168b975d666af624f411386cb
wa1tnr <wa1tnr@users.noreply.github.com>
2021-12-28 23:54:43 +0000

    experiment untested gl

--- ArduinoIDE/fm0x/simple_adrs_prn/rev-aa.ino
+++ ArduinoIDE/fm0x/simple_adrs_prn/rev-aa.ino
@@ -8,6 +8,8 @@
 // internal flashROM address:
 #define START_ADDRESS 0x1d20
 
+
+
 // how much output - in units of 16 byte lines:
 #define LINES 8
 
@@ -20,6 +22,103 @@ char buffer[128];
 // simple putch() as used in the C language:
 void putch(char ch) {
     Serial.print(ch);
+}
+
+
+
+
+
+
+#define lcl_printf() \
+    buf_ptr = * & buffer; \
+    memcpy(buffering, buf_ptr, sizeof buffer); \
+    print_buffer();
+
+char buffering[64];
+
+void print_buffer(void) {
+    Serial.print(buffering);
+}
+
+void space_it(void) {
+      sprintf(buffering, "%c", ' ');
+      print_buffer();
+}
+
+void newline(void) {
+      sprintf(buffering, "%c", '\n');
+      print_buffer();
+}
+
+void testpa(void) {
+      newline();
+      space_it();
+      
+      char buffer[48]; // 32 also 64
+      char* buf_ptr;
+
+      buffer[0] = 'a';
+      buffer[1] = 'b';
+      buffer[2] = 'c';
+      buffer[3] = '\000';
+
+      buf_ptr = buffer;
+
+      int buf_size, buf_ptr_size;
+
+      buf_size = sizeof(buffer); // captures "abc\000" size
+      buf_ptr_size = sizeof(buf_ptr);
+
+      int buf_len = strlen(buffer);
+
+      size_t gottem;
+
+      sprintf(buffering, "%c", '\'');
+      print_buffer();
+
+      memcpy(buffering, buf_ptr, sizeof buffer);
+      print_buffer();
+
+      sprintf(buffering, "%c%c", '\'', ' ');
+      print_buffer();
+
+      sprintf(buffering, "%s ", " is the buffer contents");
+      print_buffer();
+
+      sprintf(buf_ptr, "\n         sizeof(buf_ptr) is  %d", buf_ptr_size);
+
+      lcl_printf();
+
+      sprintf(buf_ptr, "%s", "\n         sizeof(buffer)  is ");
+
+      lcl_printf();
+
+      buf_size = sizeof(buffer); // captures "abc\000" size
+
+      sprintf(buf_ptr, "%d\n", buf_size); // related to string length, possibly
+      lcl_printf();
+
+      sprintf(buf_ptr, "%s", "         strlen(buffer)  is ");
+      lcl_printf();
+
+      sprintf(buf_ptr, " %d\n", buf_len); // related to string length, possibly
+      lcl_printf();
+
+      uint8_t adrs;
+      adrs = (uint8_t) & buf_ptr;
+
+      sprintf(buf_ptr, "%s", "adrs (& buf_ptr) in hex is        ");
+      lcl_printf();
+
+      // print the buffer's address in ram
+      sprintf(buf_ptr, "0x%.8X\n", adrs);
+      lcl_printf();
+
+      sprintf(buf_ptr, "%s", "adrs (& buf_ptr) in decimal is  ");
+      lcl_printf();
+
+      sprintf(buf_ptr, "%c%.11u\n\n", ' ', adrs);
+      lcl_printf();
 }
 
 int dump_16_bytes(void) {
@@ -77,6 +176,10 @@ void setup (void) {
 
     Serial.println("here is.");
 
+    testpa();
+    
+    
+    
     for (int index = LINES; index > 0; index--) { // dump three lines 16 bytes/line
         // p has an initial value the first time through this loop
         // p is an address in RAM or ROM
